d1895e1277154f2eac1c7e3851aef662be87bc5d
Suhrullekha <suhruulekha@gmail.com>
2021-02-17 20:04:55 -0500

    first react component

--- front/src/App.js
+++ front/src/App.js
@@ -3,8 +3,21 @@
 
 function App() {
     return (
-        <div className="App">
-
+        <div className="app">
+            <h1 class="title">Spyrise</h1>
+            <div class="buttons">
+                <div class="row1">
+                    <button>Create Room</button>
+                    <button>Join Room</button>
+                </div>
+                <div class="row2">
+                    <button>How to Play</button>
+                </div>
+                <div class="row3">
+                    <button>Quit</button>
+                </div>
+            </div>
+            <img src="spy128.png" width={30} height={30} />
         </div>
     );
 }
